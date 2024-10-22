82187c41b16bff0a46efc06892d4a39c7099be3e
amaldre <alexandre.malfoy.62@gmail.com>
2024-10-22 13:48:42 +0200

    affichage malfoy

--- affichage_malfoy/pages/page_1.py
+++ affichage_malfoy/pages/page_1.py
@@ -8,20 +8,14 @@ from collections import Counter
 import logging
 
 from class_block_st import Block
-
-spices = ['salt', 'garlic', 'pepper', 'paprika', 'basil', 'lime', 'cumin', 'garlic'] # Common spices to exclude from list of ingredients
-common_ingredients = ['water', 'flour', 'baking powder','cornstarch'] # Because water and flour don't have important nutritional values
-alcohol = ['vodka', 'ice', 'beer']
-
-filtre = spices+common_ingredients+alcohol
+import functions
 
 @st.cache_data
-def load_data():
-    # Simuler un chargement de données lourd
-    data = pd.read_csv("RAW_recipes.csv")
+def load_data(df_path):
+    data = pd.read_csv(df_path)
     return data
 
-RAW_recipes = load_data()
+RAW_recipes = load_data("../data/RAW_recipes.csv")
 
 RAW_recipes[['calories','total fat (%)','sugar (%)','sodium (%)','protein (%)','saturated fat (%)','carbohydrates (%)']] = RAW_recipes.nutrition.str.split(",",expand=True)
 RAW_recipes['calories'] = RAW_recipes['calories'].apply(lambda x: x.replace('[','')) 
@@ -34,55 +28,18 @@ RAW_recipes['sodium (%)'] = RAW_recipes['sodium (%)'].apply(ast.literal_eval)
 RAW_recipes['protein (%)'] = RAW_recipes['protein (%)'].apply(ast.literal_eval)
 RAW_recipes['saturated fat (%)'] = RAW_recipes['saturated fat (%)'].apply(ast.literal_eval)
 RAW_recipes['carbohydrates (%)'] = RAW_recipes['carbohydrates (%)'].apply(ast.literal_eval)
-
 RAW_recipes['ingredients_cleaned'] = RAW_recipes['ingredients'].str.lower().str.strip()
 
+spices = ['salt', 'garlic', 'pepper', 'paprika', 'basil', 'lime', 'cumin', 'garlic'] # Common spices to exclude from list of ingredients
+common_ingredients = ['water', 'flour', 'baking powder','cornstarch'] # Because water and flour don't have important nutritional values
+alcohol = ['vodka', 'ice', 'beer']
+filtre = spices+common_ingredients+alcohol
+
 list_option = ['calories','total fat (%)', 'sugar (%)']
+list_df = ['RAW_recipes','RAW_recipes']
 
-if 'arg' not in st.session_state:
-    st.session_state.arg = 'calories'
-if 'valeur' not in st.session_state:
-    st.session_state.valeur = 0
+with st.form('form_df'):
+    df_choose = st.selectbox(label='Choose', options = list_df, key='df_choose')
+    submit_button1 = st.form_submit_button('Set Dataframe')
 
-with st.form('form1'):
-    arg = st.selectbox(label='Choose', options = list_option, key='arg')
-    submit_button1 = st.form_submit_button('Set Argument')
-
-with st.form('form2'):
-    if st.session_state.arg == 'calories':
-        valeur = st.slider('Valeur', 0, 1000, st.session_state.valeur, 500)
-    else:
-        valeur = st.slider('Valeur', 0, 100, st.session_state.valeur, 50)
-    submit_button = st.form_submit_button('$+$')
-    if submit_button:
-        st.session_state.valeur = valeur
-
-if 'graphs' not in st.session_state:
-    st.session_state.graphs = []
-
-if submit_button:
-    my_block = Block(RAW_recipes, arg, valeur, filtre)
-    fig = my_block.graph(my_block.dataframe, my_block.argument, valeur, filtre)
-    st.session_state.graphs.append(fig)
-
-#if st.session_state.graphs:
-    #st.write("Voici les graphes sauvegardés dans session_state :")
-    #for graph in st.session_state.graphs:
-        #st.pyplot(graph)
-
-if st.session_state.graphs:
-    st.write("Graph à supprimer :")
-
-    graphs_to_display = st.session_state.graphs.copy()
-
-    for idx, fig in enumerate(graphs_to_display):
-        if st.button(f'Supprimer le graphique {idx + 1}', key=f'remove_{idx}'):
-            st.session_state.graphs.pop(idx)
-            st.rerun()
-            break
-    if st.session_state.graphs:
-        st.write("Graphiques affichés :")
-        for remaining_fig in st.session_state.graphs:
-            st.pyplot(remaining_fig)
-else:
-    st.write("Aucun graphique sauvegardé.")
+functions.main(RAW_recipes, list_option, filtre)
