4985a3ce215dcac2d58b2dd33df54231092e369a
Damien <damien@damien-Unbuntu-24.04>
2024-11-04 14:03:37 +0100

    ajout du filtre sur date

--- src/classes.py
+++ src/classes.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import math
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 class Study:
 
@@ -47,6 +48,7 @@ class Study:
         col1, col2 = st.columns(2)
         with col1: 
             start_date = st.date_input("Start date")
+            print(start_date)
         with col2:
             end_date = st.date_input("End date")
         return start_date, end_date
@@ -54,6 +56,7 @@ class Study:
     def get_data_points(self, df, axis_x, axis_y, range_axis_x, range_axis_y, chosen_filters, range_filters):
         columns = [axis_x,axis_y,"id"] + [filtre for filtre in chosen_filters if (filtre != axis_x and filtre != axis_y)]
         df = df[columns].sort_values(by=axis_x)
+        
         df = df[(df[axis_x] >= range_axis_x[0]) & (df[axis_x] <= range_axis_x[1])]
         df = df[(df[axis_y] >= range_axis_y[0]) & (df[axis_y] <= range_axis_y[1])]
         if len(chosen_filters)>0:
@@ -62,79 +65,62 @@ class Study:
         return df[axis_x].values, df[axis_y].values, df["id"].values
     
 
-    def __set_range_axis(self):
-        if self.axis_y == "submitted":
-            range_axis_y = self.__set_date(self.dataframe, self.axis_y)
+    def __set_range_axis(self, axis):
+        if axis == "submitted":
+            range_axis = self.__set_date()
         else:
-            range_axis_y = self.__create_slider_from_df(self.dataframe, self.axis_y)
-        if self.axis_x == "submitted":
-            range_axis_x = self.__set_date(self.dataframe, self.axis_y)
-        else:    
-            range_axis_x = self.__create_slider_from_df(self.dataframe, self.axis_x)
-        return range_axis_x, range_axis_y
-
+            range_axis = self.__create_slider_from_df(self.dataframe, axis)
+        return range_axis
+    
     def __filters(self, axis_x, axis_y):
         filters = [filtre for filtre in self.filters if (filtre != axis_x and filtre != axis_y)]
-        chosen_filters = st.sidebar.multiselect(label ="filters", options=filters)
-        print(chosen_filters)
+        
+        chosen_filters = st.multiselect(label ="filters", options=filters)
         range_filters = []
         for filter in chosen_filters:
-            min = math.floor(self.dataframe[filter].min())
-            max = math.ceil(self.dataframe[filter].max())
-            range = st.sidebar.slider(filter, min_value=min, max_value=max, value=(min,max))
-            print(range)
-            range_filters.append(range)
-            print(range_filters)
-        print(range_filters)
+            range_axis = self.__set_range_axis(filter)
+            range_filters.append(range_axis)
+
         return chosen_filters,range_filters
 
     def display_graph(self):
+        chosen_filters = []
+        range_filters = []
         # Generate data
-        print("début_display",self.delete)
-        if self.delete ==False:
-            
-            with st.form(self.key):
 
+        if self.delete ==False:
+            with st.expander(label=self.key, expanded=True):
                 self.axis_x, self.axis_y = self.__set_axis()
-                self.range_axis_x, self.range_axis_y = self.__set_range_axis()
-                chosen_filters, range_filters = self.__filters(self.axis_x, self.axis_y)
-                x, y, recipes_id = self.get_data_points(self.dataframe, 
+                self.range_axis_x = self.__set_range_axis(self.axis_x)
+                self.range_axis_y = self.__set_range_axis(self.axis_y)
+                if st.checkbox("extra filters"):
+                    chosen_filters, range_filters = self.__filters(self.axis_x, self.axis_y)
+                
+                with st.form(self.key):
+                    if st.form_submit_button(label="Draw graph"):
+                        x, y, recipes_id = self.get_data_points(self.dataframe, 
                                                         self.axis_x, 
                                                         self.axis_y, 
                                                         self.range_axis_x, 
                                                         self.range_axis_y, 
                                                         chosen_filters,
                                                         range_filters)
-            
-                if st.form_submit_button(label="Draw graph"):
+                        col = st.columns([1,3,1])
+                        with col[1]:
+                        # Create a figure
+                            fig, ax = plt.subplots(figsize=(10,6))
+                            ax.scatter(x, y, s=0.5)
+                            # Display Matplotlib figure in Streamlit
+                            st.pyplot(fig)
+                            st.write(f"number of recipes : {len(x)}")
 
-                    col = st.columns([1,3,1])
-
-                    with col[1]:
-                    # Create a figure
-                        fig, ax = plt.subplots(figsize=(10,6))
-                        ax.scatter(x, y, s=0.5)
-                        # Display Matplotlib figure in Streamlit
-                        st.pyplot(fig)
-                        st.write(f"number of recipes : {len(x)}")
-
-                    display_df = self.dataframe[self.dataframe['id'].isin(recipes_id)]
-                    display_df = display_df.sort_values(by="count_total",ascending=False)[:10]
-                    with st.expander("The 10 recipes with the most comments (with current filters)"):
+                        display_df = self.dataframe[self.dataframe['id'].isin(recipes_id)]
+                        display_df = display_df.sort_values(by="count_total",ascending=False)[:10]
                         st.dataframe(display_df,hide_index=True)
-                    
-                if st.form_submit_button(label="Delete graph"):
-                    self.delete = True
-                    print("delete",self.delete)
-                    st.rerun()
+                        
+                    if st.form_submit_button(label="Delete graph"):
+                        self.delete = True
+                        print("delete",self.delete)
+                        st.rerun()
 
 
-class TimeStudy(Study):
-
-    def __set_range_axis(self):
-        range_axis_y = self.__create_slider_from_df(self.dataframe, self.axis_y)
-        start_date = st.date_input("start date",self.dataframe["date"])
-        end_date = st.date_input("start date",self.dataframe["date"])
-
-        range_axis_x = (start_date, end_date)
-        return range_axis_x, range_axis_y
