0997e94e9525162b40c53bb6e591a69a67ccf24c
amaldre <alexandre.malfoy.62@gmail.com>
2024-11-04 11:04:59 +0100

    changement dans affichage_malfoy, plusieurs boutons, histo/boxplot/density

--- affichage_malfoy/pages/page_2.py
+++ affichage_malfoy/pages/page_2.py
@@ -24,23 +24,32 @@ def merge_df(df1, df2, column):
 if "graph" not in st.session_state:
     st.session_state["graph"] = []
 
+if "filters" not in st.session_state:
+    st.session_state["filters"] = []
+
 def main():
 
-    # print("début",len(st.session_state["graph"]))
-    if st.button("refresh"):
+    col1, col2, _, _, _, _, _, _, _, _ = st.columns(10)
+
+    with col1:
+        refresh_button = st.button("refresh")
+    with col2:
+        add_graph_button = st.button("Add Graph")
+
+    if refresh_button:
         st.rerun()
 
-    recipes_df = import_df("data/recipes_explicit_nutriments.csv")
-    mean_rating_df = import_df("data/mean_ratings.csv")
+    recipes_df = import_df("../data/recipes_explicit_nutriments.csv")
+    mean_rating_df = import_df("../data/mean_ratings.csv")
     mean_rating_df.rename(columns={"recipe_id":"id"}, inplace=True)
     idx_with_max_value = recipes_df["calories"].values.argmax()
     recipes_df = recipes_df.drop(index=idx_with_max_value)
     dataframe = merge_df(recipes_df,mean_rating_df,"id")
 
-    axis_x_list = ["count_total","mean_rating","calories","total_fat (%)","sugar (%)","sodium (%)","protein (%)","saturated_fat (%)","carbohydrates (%)"]
-    filters = ["count_total","mean_rating","calories","total_fat (%)","sugar (%)","sodium (%)","protein (%)","saturated_fat (%)","carbohydrates (%)"]
+    axis_x_list = ["count_total","mean_rating","calories","total fat (%)","sugar (%)","sodium (%)","protein (%)","saturated fat (%)","carbohydrates (%)"]
+    filters = ["count_total","mean_rating","calories","total fat (%)","sugar (%)","sodium (%)","protein (%)","saturated fat (%)","carbohydrates (%)"]
 
-    if st.button("Add Graph"):
+    if add_graph_button:
         name = f"graph {len(st.session_state["graph"]) + 1}"
         study = Study(dataframe, axis_x_list, filters, name)
         st.session_state["graph"].append(study)
