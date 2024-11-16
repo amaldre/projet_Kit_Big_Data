6e2562d63e3c54e113df8806d8ec37afd0c796a9
alexandreRocchiHenry <alexandrerocchi2@gmail.com>
2024-11-16 16:19:09 +0100

    Correction des test

--- tests/test_pipeline_preprocess.py
+++ tests/test_pipeline_preprocess.py
@@ -190,9 +190,8 @@ def test_change_na_description_by_name():
         'description': ['description1', 'description2', None]
     })
     
-    result = change_na_description_by_name(data.copy(), 'name', 'description')
-    
-    # Vérifier les valeurs
+    result = change_na_description_by_name(data.copy())
+
     assert result['description'].iloc[2] == 'Bob'
     
 
@@ -201,11 +200,11 @@ from scripts.pipeline_preprocess import delete_outliers_steps
 def test_delete_outliers_steps():
     
     data = pd.DataFrame({
-        'steps': [5, 10, 0]
+        'n_steps': [5, 10, 0]
     })
     
     result = delete_outliers_steps(data.copy())
     
     # Les deux plus grandes valeurs et les valeurs égales à 0 doivent être supprimées
     expected_steps = [5, 10]
-    assert list(result['steps']) == expected_steps
+    assert list(result['n_steps']) == expected_steps
