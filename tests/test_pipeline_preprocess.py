0cc52b550b7b9ed84ec90d2e7fb0dfa4dc6e9342
alexandreRocchiHenry <alexandrerocchi2@gmail.com>
2024-11-18 09:57:40 +0100

    test de clean and tok

--- tests/test_pipeline_preprocess.py
+++ tests/test_pipeline_preprocess.py
@@ -277,16 +277,17 @@ def test_groupby():
 
 from scripts.pipeline_preprocess import clean_and_tokenize
 
-def test_ckean_and_tokenize():
+def test_clean_and_tokenize(self):
     data = pd.DataFrame({
         'text': ['This is a test.', 'Another test!']
     })
+    stopwords = {'is', 'a', 'another'}
     
-    result = clean_and_tokenize(data.copy(), 'text')
-    
+    data['tokens'] = data['text'].apply(lambda x: clean_and_tokenize(x, stopwords))
+
     expected = pd.DataFrame({
-        'text': ['test'],
-        'tokens': [['test']]
+        'text': ['This is a test.', 'Another test!'],
+        'tokens': [['test'], ['test']]
     })
-    
-    pd.testing.assert_frame_equal(result, expected)
+
+    pd.testing.assert_frame_equal(data, expected)
