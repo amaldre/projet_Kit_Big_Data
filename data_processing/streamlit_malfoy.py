272711c7d922716aabd4ad12cb51d82000bc1b8d
alexandreRocchiHenry <alexandrerocchi2@gmail.com>
2024-11-28 14:30:50 +0100

    Black Python pep8 formating

--- data_processing/streamlit_malfoy.py
+++ data_processing/streamlit_malfoy.py
@@ -3,6 +3,7 @@ import pandas as pd
 import ast
 import time
 
+
 PP_recipe = pd.read_csv("../data/PP_recipes.csv")
 PP_users = pd.read_csv("../data/PP_users.csv")
 RAW_recipes = pd.read_csv("../data/RAW_recipes.csv")
@@ -11,29 +12,44 @@ interactions_test = pd.read_csv("../data/interactions_test.csv")
 ############ definition of the functions
 
 
-
 ############ Data analysis
 
 ### This is preliminary steps to sort the rows
 
-calories = st.sidebar.slider('Calories', 0, 1000, 500) # Slider for the calories ( in the left sidebar )
-ingredients = st.text_input('Ingredients (seperate every ingredients with a ",")') # To obtain the differents ingredients
+calories = st.sidebar.slider(
+    "Calories", 0, 1000, 500
+)  # Slider for the calories ( in the left sidebar )
+ingredients = st.text_input(
+    'Ingredients (seperate every ingredients with a ",")'
+)  # To obtain the differents ingredients
 
-words_list = ingredients.split(',')
+words_list = ingredients.split(",")
 
-ingredients_to_search = [word.strip() for word in words_list] # Put every ingredients in a vector (ex : ['basmati rice', 'water'])
+ingredients_to_search = [
+    word.strip() for word in words_list
+]  # Put every ingredients in a vector (ex : ['basmati rice', 'water'])
 
-pattern = ''.join([f'(?=.*\\b{ingredient}\\b)' for ingredient in ingredients_to_search]) # Regex expressio
+pattern = "".join(
+    [f"(?=.*\\b{ingredient}\\b)" for ingredient in ingredients_to_search]
+)  # Regex expressio
 
 # Extract the rows with the ingredients written by the user
-matching_rows = RAW_recipes[RAW_recipes['ingredients'].str.contains(pattern, case=False, na=False, regex=True)] 
+matching_rows = RAW_recipes[
+    RAW_recipes["ingredients"].str.contains(pattern, case=False, na=False, regex=True)
+]
 
-matching_rows["full_nutrition"] = matching_rows['nutrition'].apply(ast.literal_eval) # Because the nutrition column is written as a string
+matching_rows["full_nutrition"] = matching_rows["nutrition"].apply(
+    ast.literal_eval
+)  # Because the nutrition column is written as a string
 # Ex : [840.0,40.0] written as '[','8','4' etc..
-matching_rows["full_nutrition"] = matching_rows['full_nutrition'].apply(lambda x: x)
-matching_rows["calories"] = matching_rows['full_nutrition'].apply(lambda x: x[0]) # Extract the calories to sort later
+matching_rows["full_nutrition"] = matching_rows["full_nutrition"].apply(lambda x: x)
+matching_rows["calories"] = matching_rows["full_nutrition"].apply(
+    lambda x: x[0]
+)  # Extract the calories to sort later
 
-filtered_recipes = matching_rows[matching_rows['calories'] < calories] # Filter the recipes by the calories with the slider defined at the beginning
+filtered_recipes = matching_rows[
+    matching_rows["calories"] < calories
+]  # Filter the recipes by the calories with the slider defined at the beginning
 
 ############ Display of the app
 
@@ -43,8 +59,13 @@ st.dataframe(filtered_recipes.head())
 
 recipe_1 = filtered_recipes.head(1)
 
-recipe_1_description = (recipe_1["steps"].values[0])
-cleaned_text = recipe_1_description.replace("[", "").replace("]", "").replace("'", "").replace('"', "")
+recipe_1_description = recipe_1["steps"].values[0]
+cleaned_text = (
+    recipe_1_description.replace("[", "")
+    .replace("]", "")
+    .replace("'", "")
+    .replace('"', "")
+)
 cleaned_text = cleaned_text.replace(",", "\n")
 instructions_string = cleaned_text.strip()
 
--- tests/test_DBapi.py
+++ tests/test_DBapi.py
@@ -4,113 +4,122 @@ import pytest
 from unittest.mock import patch, MagicMock
 
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from src.utils.dbapi import DBapi
 
-def test_dbapi_init():
-    with patch('src.utils.dbapi.os.getenv') as mock_getenv, \
-         patch('src.utils.dbapi.MongoClient') as mock_mongo_client:
 
-        mock_getenv.return_value = 'mongodb://localhost:27017'
+def test_dbapi_init():
+    with patch("src.utils.dbapi.os.getenv") as mock_getenv, patch(
+        "src.utils.dbapi.MongoClient"
+    ) as mock_mongo_client:
+
+        mock_getenv.return_value = "mongodb://localhost:27017"
         mock_client_instance = MagicMock()
         mock_mongo_client.return_value = mock_client_instance
-
 
         db_api = DBapi()
 
         # Assertions
-        mock_getenv.assert_called_once_with('URI_DB')
-        mock_mongo_client.assert_called_once_with('mongodb://localhost:27017')
+        mock_getenv.assert_called_once_with("URI_DB")
+        mock_mongo_client.assert_called_once_with("mongodb://localhost:27017")
         assert db_api.client == mock_client_instance
         assert db_api.db == mock_client_instance["MangaTaMainDF"]
         assert db_api.collection == db_api.db["Food.com"]
 
-@patch('os.getenv')
+
+@patch("os.getenv")
 def test_dbapi_find_by(mock_getenv):
     mock_getenv.return_value = "mongodb://mock_uri"
-    with patch('src.utils.dbapi.MongoClient') as mock_mongo_client:
+    with patch("src.utils.dbapi.MongoClient") as mock_mongo_client:
         mock_collection = MagicMock()
-        mock_db = {'Food.com': mock_collection}
-        mock_client_instance = {'MangaTaMainDF': mock_db}
+        mock_db = {"Food.com": mock_collection}
+        mock_client_instance = {"MangaTaMainDF": mock_db}
         mock_mongo_client.return_value = mock_client_instance
 
         db_api = DBapi()
 
         db_api.client = mock_client_instance
-        db_api.db = mock_client_instance['MangaTaMainDF']
+        db_api.db = mock_client_instance["MangaTaMainDF"]
         db_api.collection = mock_collection
 
         # Configurer find().limit()
         mock_find = MagicMock()
         mock_collection.find.return_value = mock_find
-        mock_find.limit.return_value = ['doc1', 'doc2']
+        mock_find.limit.return_value = ["doc1", "doc2"]
 
         # Appeler la méthode
-        result = db_api.find_by('test_col', 'test_value', nb=2)
+        result = db_api.find_by("test_col", "test_value", nb=2)
 
         # Assertions
-        mock_collection.find.assert_called_once_with({'test_col': 'test_value'})
+        mock_collection.find.assert_called_once_with({"test_col": "test_value"})
         mock_find.limit.assert_called_once_with(2)
-        assert result == ['doc1', 'doc2']
+        assert result == ["doc1", "doc2"]
+
 
 def test_dbapi_find_range_submitted():
-    with patch('src.utils.dbapi.MongoClient') as mock_mongo_client:
+    with patch("src.utils.dbapi.MongoClient") as mock_mongo_client:
         # Mock de la collection
         mock_collection = MagicMock()
-        mock_db = {'Food.com': mock_collection}
-        mock_client_instance = {'MangaTaMainDF': mock_db}
+        mock_db = {"Food.com": mock_collection}
+        mock_client_instance = {"MangaTaMainDF": mock_db}
         mock_mongo_client.return_value = mock_client_instance
 
         # Créer une instance de DBapi
         db_api = DBapi()
         db_api.client = mock_client_instance
-        db_api.db = mock_client_instance['MangaTaMainDF']
+        db_api.db = mock_client_instance["MangaTaMainDF"]
         db_api.collection = mock_collection
 
         # Configurer le side_effect pour find
         def side_effect(query):
-            submitted_range = query.get('submitted')
-            return ['doc_submitted_{}'.format(i) for i in range(submitted_range['$gte'], submitted_range['$lt'])]
+            submitted_range = query.get("submitted")
+            return [
+                "doc_submitted_{}".format(i)
+                for i in range(submitted_range["$gte"], submitted_range["$lt"])
+            ]
+
         mock_collection.find.side_effect = side_effect
 
         # Appeler la méthode
         result = db_api.find_range_submitted(1, 4)
 
         # Assertions
-        expected_calls = [({'submitted': {'$gte': 1, '$lt': 4}},)]
+        expected_calls = [({"submitted": {"$gte": 1, "$lt": 4}},)]
         actual_calls = [call.args for call in mock_collection.find.call_args_list]
         assert actual_calls == expected_calls
-        expected_result = ['doc_submitted_1', 'doc_submitted_2', 'doc_submitted_3']
+        expected_result = ["doc_submitted_1", "doc_submitted_2", "doc_submitted_3"]
         assert result == expected_result
 
+
 def test_dbapi_use_query():
-    with patch('src.utils.dbapi.MongoClient') as mock_mongo_client:
+    with patch("src.utils.dbapi.MongoClient") as mock_mongo_client:
         # Mock de la collection
         mock_collection = MagicMock()
-        mock_db = {'Food.com': mock_collection}
-        mock_client_instance = {'MangaTaMainDF': mock_db}
+        mock_db = {"Food.com": mock_collection}
+        mock_client_instance = {"MangaTaMainDF": mock_db}
         mock_mongo_client.return_value = mock_client_instance
 
         # Créer une instance de DBapi
         db_api = DBapi()
         db_api.client = mock_client_instance
-        db_api.db = mock_client_instance['MangaTaMainDF']
+        db_api.db = mock_client_instance["MangaTaMainDF"]
         db_api.collection = mock_collection
 
         # Configurer find()
-        mock_collection.find.return_value = ['doc1', 'doc2']
+        mock_collection.find.return_value = ["doc1", "doc2"]
 
         # Appeler la méthode avec une requête personnalisée
-        query = {'field': 'value'}
+        query = {"field": "value"}
         result = db_api.use_query(query)
 
         # Assertions
         mock_collection.find.assert_called_once_with(query)
-        assert result == ['doc1', 'doc2']
+        assert result == ["doc1", "doc2"]
+
 
 def test_dbapi_close_connection():
-    with patch('src.utils.dbapi.MongoClient') as mock_mongo_client:
+    with patch("src.utils.dbapi.MongoClient") as mock_mongo_client:
         # Mock du client
         mock_client_instance = MagicMock()
         mock_mongo_client.return_value = mock_client_instance
