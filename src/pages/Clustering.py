8a0a5802b3811d19f036950bb9185871c0e0451d
BaptCvn <baptistecervoni@gmail.com>
2024-12-03 11:35:02 +0100

    gestion des erreurs et logging

--- src/pages/Clustering.py
+++ src/pages/Clustering.py
@@ -1,17 +1,27 @@
+import os
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import logging
 
+logger = logging.getLogger(os.path.basename(__file__))
 
+# Paramétrage de Streamlit
 st.set_page_config(page_title="Explication Prétraitement", layout="wide")
 
 def load_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    try:
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+        logger.info(f"Le fichier CSS {file_name} a été chargé avec succès.")
+    except Exception as e:
+        logger.error(f"Erreur lors du chargement du fichier CSS {file_name}: {e}")
+        st.error(f"Une erreur est survenue lors du chargement du fichier CSS. Veuillez vérifier le fichier.")
 
-
+# Charger le CSS
 load_css("style.css")
 
+# Titre et description de la page
 st.title("👨‍🍳 Clustering des recettes pour analyser les types de cuisine")
 st.write(
     """
@@ -37,68 +47,71 @@ st.write(
         Nous utilisons la librairie BERTopic pour réaliser le Clustering des recettes.
         A l'aide de BERT, un embeding est réalisé pour chaque description de recette.
         Ensuite, nous réalisons le Clustering pour identifier les différents types de cuisine.
-        Pour cela, BERTopic utilise HDBSCAN. Nous paramètrés ce dernier pour faire des cluster de taille minimum 100. 
-        De plus BERTopic utilise UMAP pour la réduction de dimension.
+        Pour cela, BERTopic utilise HDBSCAN. Nous paramètrons ce dernier pour faire des clusters de taille minimum 100. 
+        De plus, BERTopic utilise UMAP pour la réduction de dimension.
     """
 )
 
 st.write(
     """
-        Afin d'obtenir les meilleurs résultats possibles et après plusieurs essaies,
-        nous avons décidé de fournir à BERTopic une 'topic_seeds' avec des mots clés généré par chatGPT. 
-        """
+        Afin d'obtenir les meilleurs résultats possibles et après plusieurs essais,
+        nous avons décidé de fournir à BERTopic une 'topic_seeds' avec des mots clés générés par chatGPT. 
+    """
 )
 
-
 def create_scrolling_banner(texte: str):
-    scrolling_banner = (
-        """
-    <style>
+    try:
+        scrolling_banner = (
+            """
+        <style>
+        .scrolling-banner {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            background-color: #f0f2f6; /* Couleur de fond du bandeau */
+            overflow: hidden;
+            height: 50px; /* Hauteur du bandeau */
+            z-index: 9999; /* Assure que le bandeau reste au-dessus des autres éléments */
+        }
 
-    .scrolling-banner {
-        position: fixed;
-        top: 0;
-        width: 100%;
-        background-color: #f0f2f6; /* Couleur de fond du bandeau */
-        overflow: hidden;
-        height: 50px; /* Hauteur du bandeau */
-        z-index: 9999; /* Assure que le bandeau reste au-dessus des autres éléments */
-    }
-
-    .scrolling-banner h1 {
-        position: absolute;
-        width: 100%;
-        height: 50px;
-        line-height: 50px;
-        margin: 0;
-        font-size: 24px;
-        color: #4CAF50; /* Couleur du texte */
-        text-align: center;
-        transform: translateX(100%);
-        animation: scroll-left 10s linear infinite;
-    }
-
-    /* Animation pour le défilement du texte */
-    @keyframes scroll-left {
-        from {
+        .scrolling-banner h1 {
+            position: absolute;
+            width: 100%;
+            height: 50px;
+            line-height: 50px;
+            margin: 0;
+            font-size: 24px;
+            color: #4CAF50; /* Couleur du texte */
+            text-align: center;
             transform: translateX(100%);
+            animation: scroll-left 10s linear infinite;
         }
-        to {
-            transform: translateX(-100%);
+
+        /* Animation pour le défilement du texte */
+        @keyframes scroll-left {
+            from {
+                transform: translateX(100%);
+            }
+            to {
+                transform: translateX(-100%);
+            }
         }
-    }
-    </style>
+        </style>
 
-    <div class="scrolling-banner">
-        <h1>"""
-        + texte
-        + """</h1>
-    </div>
-    """
-    )
+        <div class="scrolling-banner">
+            <h1>"""
+            + texte
+            + """</h1>
+        </div>
+        """
+        )
+        logger.info("Bannière défilante créée avec succès.")
+        return scrolling_banner
+    except Exception as e:
+        logger.error(f"Erreur lors de la création de la bannière défilante: {e}")
+        st.error("Une erreur est survenue lors de la création de la bannière défilante.")
 
-    return scrolling_banner
-
-
-scrolling_banner = create_scrolling_banner("Texte à faire défiller")
+# Affichage de la bannière défilante
+scrolling_banner = create_scrolling_banner("Texte à faire défiler")
 st.components.v1.html(scrolling_banner, height=60)
+
